e811bd7363d05cc8ea39f3856ea1d112fcecaabf
Elias Bouali <ebouali@fu-r9-p4.s19.be>
2025-04-12 14:18:48 +0200

    libft final

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ebouali <ebouali@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 19:56:27 by ebouali           #+#    #+#             */
-/*   Updated: 2025/04/11 21:39:26 by ebouali          ###   ########.fr       */
+/*   Updated: 2025/04/12 14:18:23 by ebouali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,3 +60,26 @@ int main(void)
     return (0);
 }
 */
+
+int     ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+        const unsigned char *str1;
+        const unsigned char *str2;
+        size_t i;
+
+        str1 = (const unsigned char *)s1;
+        str2 = (const unsigned char *)s2;
+        i = 0;
+        while (i < n)
+        {
+                if (*str1 > *str2)
+                        return (1);
+                else if (*str1 < *str2)
+                        return (-1);
+                str1++;
+                str2++;
+                i++;
+        }
+        return (0);
+}
+
